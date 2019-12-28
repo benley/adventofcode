7c0405c2a4110a663235ad47806cc5a6a718a49e
Benjamin Staffin <benley@gmail.com>
2019-12-28 15:46:36 -0500

    2019 day 6

--- 2019/D6/D6.hs
+++ 2019/D6/D6.hs
@@ -3,24 +3,15 @@
 module D6_2019 where
 
 import Prelude hiding (lines, readFile)
-import Data.Map as M
-import Data.Maybe (fromJust)
+import qualified Data.Map as M
+import Data.Maybe
 import Data.Text (lines, splitOn, Text)
 import Data.Text.IO (readFile)
-import Data.Set as Set
-import Data.Graph.Inductive.Graph as G
--- import Data.Graph.Inductive.Query.GVD as GVD
+import qualified Data.Set as Set
+import Data.Graph.Inductive.Graph
+import Data.Graph.Inductive.Basic (undir)
 import Data.Graph.Inductive.PatriciaTree
-import Data.Graph.Inductive.Query.SP as SP
-
-
--- | generate list of labeled nodes
--- genLNodes :: (Enum a) => a -> Int -> [LNode a]
--- genLNodes q i = take i (zip [1..] [q..])
-
--- | denote unlabeled edges
--- labUEdges :: [Edge] -> [UEdge]
--- labUEdges = Prelude.map (\(i,j) -> (i, j, ()))
+import Data.Graph.Inductive.Query.SP (spLength)
 
 main :: IO ()
 main = do
@@ -38,29 +29,18 @@ main = do
 
     getNodeID = fromJust . flip M.lookup nodeIDs
 
-    myEdges = Prelude.map (\[orbited, orbiting] -> ( getNodeID orbiting
-                                                   , getNodeID orbited
-                                                   , 1 )
-                          ) input
+    myEdges = map (\[orbited, orbiting] -> (getNodeID orbiting, getNodeID orbited, 1))
+                  input
     g :: Gr Text Int
-    g = mkGraph nodesById myEdges
+    g = undir $ mkGraph nodesById myEdges
 
-    rootNode = fromJust $ M.lookup "COM" nodeIDs
+    -- for part 1
+    rootNode = getNodeID "COM"
+    allOrbits = sum $ catMaybes [spLength n rootNode g | n <- nodes g]
 
-    -- v = gvdIn [rootNode] g
-  -- prettyPrint g
+    -- for part 2
+    (youNode, santaNode) = (getNodeID "YOU", getNodeID "SAN")
+    orbitalTransfers = fromJust (spLength youNode santaNode g) - 2
 
-  print $ "Order: " ++ show (G.order g)
-
-  -- print $ G.suc g 123
-  print $ "rootnode ID: " ++ show rootNode
-  -- print v
-
-  print $ SP.spLength 1058 376 g
-
--- dag3 :: Gr Char ()
--- dag3 = mkGraph (Prelude.zip [1..3] "abc") (labUEdges [(1,3)])
--- -- dag4 :: Gr Int ()
--- -- dag4 = mkGraph (genLNodes 1 4) (labUEdges [(1,2),(1,4),(2,3),(2,4),(4,3)])
--- asdf :: Gr String ()
--- asdf = mkGraph [(1, "bar")] [(1, 2, ())]
+  putStrLn $ "Part 1: " ++ show allOrbits
+  putStrLn $ "Part 2: " ++ show orbitalTransfers

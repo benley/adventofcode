6f1cf4be3e586989b362535c78f4ebb3340155a1
Benjamin Staffin <benley@gmail.com>
2021-06-26 13:52:01 -0400

    WIP I don't remember what I was even doing here

--- Aoc2020/D1.hs
+++ Aoc2020/D1.hs
@@ -1,10 +1,36 @@
+{-# LANGUAGE TupleSections #-}
+
 module Main where
 
 import Data.List (sort)
 import qualified Data.Set as Set
 
-main = do
+part1 = do
   nums <- sort . map read . lines <$> readFile "Aoc2020/input1.txt"
   let checkN n = n < 1000 && (2020 - n) `Set.member` Set.fromAscList nums
       firstMatch = head $ filter checkN nums
   print (firstMatch * (2020 - firstMatch))
+
+
+-- pairs :: [a] -> [(a, a)]
+pairs (x1:xs) = map (x1,) xs ++ pairs xs
+pairs _ = []
+
+getNums :: IO [Int]
+getNums = sort . map read . lines <$> readFile "Aoc2020/input1.txt"
+
+asdf ((n1, n2) : ns) (x : xs) =
+
+part2 = do
+  ps <- pairs <$> getNums
+  let ps_ = filter ( \(x, y) -> x+y < 2020 ) ps
+      -- asdf :: (Int, Int) -> [Int]
+      -- asdf (n1, n2) = filter (\n3 -> (n1+n2+n3) == 2020)
+
+  print $ length ps
+  print $ length ps_
+
+
+main = do
+  part1
+  part2

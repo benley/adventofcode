4d34b67ea38d16f3dee13ce840067754c17ecbcd
Benjamin Staffin <bstaffin@singlestore.com>
2023-06-20 17:08:29 -0400

    TIL Data.List.transpose exists

--- Aoc2022/D8/D8.hs
+++ Aoc2022/D8/D8.hs
@@ -2,17 +2,11 @@
 
 module Main (main) where
 
-import Data.List (foldl')
+import Data.List (foldl', transpose)
 import Data.List.Split (chunksOf)
 import Data.Set qualified as Set
 
 type IdTree = (Int, Int)
-
-row :: Int -> [a] -> a
-row n = (!! n)
-
-col :: Int -> [[a]] -> [a]
-col n = map (row n)
 
 digit :: Char -> Int
 digit c = read [c]
@@ -28,14 +22,10 @@ main = do
   grid <- map (map digit) . lines <$> readFile "D8/input.txt"
   let
     width = length (head grid)
-
     -- give each tree a unique ID
-    idGrid :: [[IdTree]]
-    idGrid = chunksOf width $ zip [0..] $ concat grid
+    rows = chunksOf width $ zip [0..] $ concat grid
+    cols = transpose rows
+    visibleByRow = concatMap visible rows ++ concatMap (visible . reverse) rows
+    visibleByCol = concatMap visible cols ++ concatMap (visible . reverse) cols
 
-    visibleByRow = concat $ map visible idGrid ++ map (visible . reverse) idGrid
-    inverted = [col n idGrid | n <- [0..width - 1]]
-    visibleByCol = concatMap visible inverted ++ concatMap (visible . reverse) inverted
-    result = Set.fromList (visibleByRow ++ visibleByCol)
-
-  print (length result)
+  print $ length $ Set.fromList $ visibleByRow ++ visibleByCol

d7437758e0e7503e6ab5befb4c6a191b51136dbf
Benjamin Staffin <benley@gmail.com>
2024-09-10 01:03:44 -0400

    cleanup, and 2023 day 1

--- .dir-locals.el
+++
@@ -1,1 +0,0 @@
-;;((haskell-mode . ((flycheck-checker . haskell-ghc))))
---
+++ Aoc2023/src/D1/D1.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Data.Char (isDigit)
+import Data.Text (filter, head, last, lines, Text, isPrefixOf, drop, take)
+import Data.Text.IO (readFile)
+import Prelude hiding (filter, head, last, lines, readFile, drop, take)
+
+filterDigits :: Text -> Int
+filterDigits t =
+  read [head ds, last ds]
+  where ds = filter isDigit t
+
+main :: IO ()
+main = do
+  input <- lines <$> readFile "src/D1/input.txt"
+  print $ part1 input
+  print $ part2 input
+
+part1, part2 :: [Text] -> Int
+part1 = sum . map  filterDigits
+part2 = sum . map (filterDigits . fixPrefix)
+
+fixPrefix :: Text -> Text
+fixPrefix "" = ""
+fixPrefix x
+  | isDigit (head x) = take 1 x <> xs
+  | "one"   `isPrefixOf` x = "1" <> xs
+  | "two"   `isPrefixOf` x = "2" <> xs
+  | "three" `isPrefixOf` x = "3" <> xs
+  | "four"  `isPrefixOf` x = "4" <> xs
+  | "five"  `isPrefixOf` x = "5" <> xs
+  | "six"   `isPrefixOf` x = "6" <> xs
+  | "seven" `isPrefixOf` x = "7" <> xs
+  | "eight" `isPrefixOf` x = "8" <> xs
+  | "nine"  `isPrefixOf` x = "9" <> xs
+  | otherwise = xs
+  where xs = fixPrefix (drop 1 x)
